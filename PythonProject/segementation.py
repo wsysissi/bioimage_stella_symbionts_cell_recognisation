c223ab92e68ea2ec8bb748cb488181b03b4be318
u5644731 <1909920063@qq.com>
2026-04-29 23:22:03 +0100

    .ds_store error

---
+++ PythonProject/segementation.py
@@ -0,0 +1,186 @@
+from segment_anything import SamPredictor, SamAutomaticMaskGenerator, sam_model_registry
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+import os
+import glob
+import skimage
+from skimage.transform import resize
+import argparse
+import datetime
+
+
+def segment_images(args, image_name, destination_folder):
+
+    cnt = 0
+    image = cv2.imread(os.path.join(root_folder,image_name))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image_orig = image
+
+    if args.isresize == True:
+        width = int(image.shape[1] * args.resize_factor)
+        height = int(image.shape[0] * args.resize_factor)
+        dim = (width, height)
+        # resize image
+        image = cv2.resize(image, dim, interpolation=cv2.INTER_NEAREST)
+
+    if not os.path.isdir(os.path.join(destination_folder)):
+        os.makedirs(os.path.join(destination_folder))
+
+    # Check if image already processed
+    all_masks_name = f'{destination_folder}/{os.path.splitext(image_name)[0]}_*.png'
+    print(f'Name: {image_name}')
+    processed_imgs = glob.glob(all_masks_name)
+    if len(processed_imgs) > 0:
+        print('Image already processed!')
+    if len(processed_imgs) == 0:
+        # Generate the segmentation masks
+        masks = mask_generator.generate(image)
+        print(f'Masks detected: {str(len(masks))}')
+        for i in range(len(masks)):
+            image_new = image_orig.copy()
+            bool_mask = masks[i]['segmentation']
+            if args.isresize == True:
+                bool_mask = resize(bool_mask, (image_new.shape[0], image_new.shape[1]))
+
+            # White background
+            image_new[bool_mask == False] = [255, 255, 255]
+            image_new = cv2.cvtColor(image_new, cv2.COLOR_RGB2BGR)
+
+            area = masks[i]['area']
+            # Change area threshold ratio acccording to your requirements
+            area_thresh = image.shape[0] * image.shape[1] * args.area_thresh_ratio
+
+            print("area:", area)
+            print("area_thresh:", area_thresh)
+            print("corner value:", bool_mask[10, 10])
+
+            # Save image if area is bigger than a specified threshold
+            if area > area_thresh:
+                # We assume, that there are no spores etc. at the corners of the images.
+                # If the mask is at the corner of the images we know that the mask is the background mask and do not save it.
+                # Use coordinates [10,10] instead of [0,0], because the model sometimes has problems with the image edges and labels the first few pixel rows incorrect
+                #if bool_mask[10, 10] == False:
+                cv2.imwrite(f'{destination_folder}/{os.path.splitext(image_name)[0]}_{str(i)}.png',
+                                image_new)
+                cnt += 1
+                print(f'Number of analyzed Images: {str(cnt)}')
+
+    # Sometimes the models label everything but the object of interest.
+    # So if still no mask is detected, iteration is done over the "negative" masks.
+    processed_imgs = glob.glob(all_masks_name)
+    if len(processed_imgs) == 0:
+        print('Computing reverse mask: ')
+        image_new = image_orig.copy()
+        background_mask = np.full((image_new.shape[0], image_new.shape[1]), False)
+
+        for i in range(len(masks)):
+            if args.isresize == True:
+                mask = resize(masks[i]['segmentation'], (image_new.shape[0], image_new.shape[1]))
+            else:
+                mask = masks[i]['segmentation']
+
+            mean_val = cv2.mean(image_new, mask.astype(np.uint8))
+            mean_val = np.mean(mean_val[0:2])
+
+            # We add the white (light) background to subtract it from the image later
+            if mean_val > 200:
+                background_mask = background_mask + mask
+
+        # White background
+        if args.isresize == True:
+            background_mask = resize(background_mask, (image_new.shape[0], image_new.shape[1]))
+        object_mask = ~background_mask
+
+        '''# Just keep largest mask
+        labeled_image, count = skimage.measure.label(object_mask, return_num=True)
+        object_features = skimage.measure.regionprops(labeled_image)
+        object_areas = [objf["area"] for objf in object_features]
+        for object_id, objf in enumerate(object_features, start=1):
+            if objf["area"] < max(object_areas):
+                labeled_image[labeled_image == objf["label"]] = False
+            if objf["area"] == max(object_areas):
+                labeled_image[labeled_image == objf["label"]] = True'''
+
+        labeled_image = skimage.measure.label(object_mask)
+        object_features = skimage.measure.regionprops(labeled_image)
+
+        min_area = image.shape[0] * image.shape[1] * args.area_thresh_ratio
+        keep_mask = np.zeros_like(object_mask, dtype=bool)
+
+        for objf in object_features:
+            if objf.area >= min_area:
+                keep_mask[labeled_image == objf.label] = True
+
+        image_new = image_orig.copy()
+        image_new[~keep_mask] = [255, 255, 255]
+        image_new = cv2.cvtColor(image_new, cv2.COLOR_RGB2BGR)
+
+        save_path = f'{destination_folder}/{os.path.splitext(image_name)[0]}_revmask.png'
+        cv2.imwrite(save_path, image_new)
+
+        image_new[labeled_image == False] = [255, 255, 255]
+        image_new = cv2.cvtColor(image_new, cv2.COLOR_RGB2BGR)
+        area_all = 0
+        for j in range(len(masks)):
+            area_all = area_all + masks[j]['area']
+        area = image.shape[0] * image.shape[1] - area_all
+        area_thresh = image.shape[0] * image.shape[1] * args.area_thresh_ratio
+        # Save image if area is bigger than a specified threshold
+        if area > area_thresh:
+            # We assume, that there are no spores etc. at the corners of the images.
+            # If the mask is at the corner of the images we know that the mask is the background mask and do not save it.
+            # Use coordinates [10,10] instead of [0,0], because the model sometimes has problems with the image edges and labels the first few pixel rows incorrect
+            #if background_mask[10, 10] == True:
+                # image_new = remove_scale(image_new)
+            cv2.imwrite(f'{destination_folder}/{os.path.splitext(image_name)[0]}_{str(i)}_revmask.png',
+                            image_new)
+            cnt += 1
+            print(f'Number of analyzed Images: {str(cnt)}')
+    # Delete loaded images to release memory and prevent loop from slowing dowm
+    del image
+    if len(processed_imgs) == 0:
+        del image_new, image_orig
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("AutoSeg")
+    parser.add_argument("--isresize", type=bool, default=False, help="Specify if images should be resized to increase speed (output has original size again)")
+    parser.add_argument("--resize_factor", type=int, default=0.5, help="Resize Factor. Height and width of images is multiplied by this factor if isresize = True")
+    parser.add_argument("--area_thresh_ratio", type=int, default=0.00005, help="Ratio that defines the minimun area a mask must have to be recognized (area_tresh_ratio = min_mask_area/total_image_area).")
+    args = parser.parse_args()
+
+    # Data path
+    # Select your own data path! To try the script there are some images stored under "./imgs/set_1/"
+    root_folder = "/Users/u5644731/Documents/bioimage_hakathon/origin"
+    # Path to store the segmented images
+    destination_folder = "/Users/u5644731/Documents/bioimage_hakathon/segmented"
+
+    # Model path
+    ckpt_vit_b = "/Users/u5644731/Downloads/sam_vit_b_01ec64.pth"
+    #ckpt_vit_h = ("/Users/u5644731/Downloads/sam_vit_h_4b8939.pth")
+
+    # Init selected model
+    sam = sam_model_registry["vit_b"](checkpoint=ckpt_vit_b)
+    predictor = SamPredictor(sam)
+    '''mask_generator = SamAutomaticMaskGenerator(sam)'''
+    mask_generator = SamAutomaticMaskGenerator(
+        model=sam,
+        points_per_side=64,
+        pred_iou_thresh=0.6,
+        stability_score_thresh=0.6,
+        crop_n_layers=1,
+        crop_n_points_downscale_factor=2,
+        min_mask_region_area=20
+    )
+
+    # Iterate through folder
+    for image_name in os.listdir(root_folder):
+        #subdir in os.listdir(root_folder):
+        #subdir_path = os.path.join(root_folder, subdir)
+        #for image_name in os.listdir(subdir_path):
+        print(datetime.datetime.now())
+        print(image_name)
+        segment_images(args, image_name, destination_folder)
+        segment_images(args, image_name, destination_folder)
+
+    #segment_images(args,"CC7_20x_1_C2_red.png",destination_folder)
